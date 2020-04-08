9b21526536061cdaa35caf107c8bfe174de9ccd8
Hans Wustrack <hans2792@gmail.com>
2020-04-08 14:19:42 +1200

    cleanup

--- script.py
+++ script.py
@@ -8,21 +8,7 @@ import pymysql
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
 
-logging.info("Hello world")
-
-test_sec = os.environ['TEST_SECRET']
-logging.info(test_sec)
-logging.info(type(test_sec))
-
-db_config = os.environ['RDS_CONFIG']
-if db_config:
-    logging.info("Got rds config")
-else:
-    logging.info("Rds config empty. Exiting.")
-    sys.exit()
-    
-logging.info(type(db_config))
-db_config = json.loads(db_config)
+db_config = json.loads(os.environ['RDS_CONFIG'])
 
 try:
     conn = pymysql.connect(db_config['host'], user=db_config['username'], passwd=db_config['password'], db=db_config['dbname'], connect_timeout=5)
